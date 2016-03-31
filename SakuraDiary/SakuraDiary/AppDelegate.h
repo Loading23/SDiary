5b5a49e45da1811ab856fbe27be20770e394ca90
Loading023 <376569230@qq.com>
2016-03-31 17:29:22 +0800

    创建项目

---
+++ SakuraDiary/SakuraDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SakuraDiary
+//
+//  Created by Loading张 on 16/3/31.
+//  Copyright © 2016年 load. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
